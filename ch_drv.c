202325a8827922fbf37fa7c5b91b3582d396accf
sonya <kanukova.s.a@gmail.com>
2018-10-21 11:21:15 +0300

    fixed

--- ch_drv.c
+++ ch_drv.c
@@ -17,6 +17,7 @@ static struct class *cl;
 
 static char device_buffer[BUFFER_SIZE]; 
 static struct file *wf;
+char file_name[255]; 
 
 static int my_open(struct inode *i, struct file *f)
 {
@@ -51,40 +52,8 @@ static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off
  	return ret;
 }
 
-static ssize_t my_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
+int open_file(size_t len) 
 {
-	printk(KERN_INFO "Driver: write()\n");
-	
-	if (!copy_from_user(device_buffer, buf, len))
-	{
-		return -1;
-	}
-	
-	if (strncmp(device_buffer, "open", 4)) 
-	{
-		if(!open_file()) {
-			printk(KERN_INFO "Driver: can't open file \"%s\"\n", file_name);
-			return -1;
-		}
-	}
-	else if(strncmp(device_buffer, "close", 5))
-	{
-		filp_close(wf, NULL);
-	}
-	else 
-	{
-		if(!write_spaces_count()) {
-			printk(KERN_INFO "Driver: write error");
-			return -1;
-		}
-	}
-
-	return len;
-}
-
-int open_file() 
-{
-	char file_name[255]; 
 	mm_segment_t oldfs;
 	
 	strncpy(file_name, device_buffer + 5, len - 6);
@@ -104,7 +73,7 @@ int open_file()
 	return 0;
 }
 
-int write_spaces_count() 
+int write_spaces_count(size_t len, loff_t *off) 
 {
 	int i = 0;
 	int space_count = 0;
@@ -129,6 +98,37 @@ int write_spaces_count()
 
 	set_fs(oldfs);
 	return 0;
+}
+
+static ssize_t my_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
+{
+	printk(KERN_INFO "Driver: write()\n");
+	
+	if (!copy_from_user(device_buffer, buf, len))
+	{
+		return -1;
+	}
+	
+	if (strncmp(device_buffer, "open", 4)) 
+	{
+		if(!open_file(len)) {
+			printk(KERN_INFO "Driver: can't open file \"%s\"\n", file_name);
+			return -1;
+		}
+	}
+	else if(strncmp(device_buffer, "close", 5))
+	{
+		filp_close(wf, NULL);
+	}
+	else 
+	{
+		if(!write_spaces_count(len, off)) {
+			printk(KERN_INFO "Driver: write error");
+			return -1;
+		}
+	}
+
+	return len;
 }
 
 static struct file_operations mychdev_fops =
